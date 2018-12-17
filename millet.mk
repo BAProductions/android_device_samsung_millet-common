3770f450174003b1f44882a6498cd215dff9505a
DJABHipHop <castellanos.kevin@rocketmail.com>
2018-12-16 21:11:46 -0500

    Removing audio_platform_info & audio_policy to board repo
    Removing audio_platform_info & audio_policy to board repo as all files are the same

--- millet.mk
+++ millet.mk
@@ -33,8 +33,6 @@ PRODUCT_COPY_FILES += \
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
-    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
 # Media
